234c411e399cbd877174a89d97f8d0bd172cc346
LaurieMcCulloch <laurie.mcculloch@ntlworld.com>
2017-07-28 15:29:29 +0100

    deltaDNA SDK added
    Added deltaDNA Unity SDK v4.5.0

---
+++ Assets/Scripts/InboxTutorial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA; 
+
+public class InboxTutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // Enter additional configuration here
+        DDNA.Instance.ClientVersion = "v1.0.0";
+        DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+
+        // Launch the SDK
+        DDNA.Instance.StartSDK(
+            "51623112369231521871823313815012",
+            "https://collect11781ttrln.deltadna.net/collect/api",
+            "https://engage11781ttrln.deltadna.net"
+        );
+
+        
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
